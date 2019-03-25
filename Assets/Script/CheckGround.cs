a63ef91a4719f9562955c154450df66b5533dd65
Kalisaria <danaamey@hotmail.com>
2019-03-25 06:29:53 +0100

    añadido menu pausa y score

---
+++ Assets/Script/CheckGround.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour {
+
+
+    private Player player;
+    
+
+
+	// Use this for initialization
+	void Start () {
+
+        player = GetComponentInParent<Player>();
+	}
+
+     void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Floor")
+        {
+            player.grounded = true;
+        }
+    }
+
+	void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Floor")
+        {
+            player.grounded = false;
+        }
+	}
+}
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour{ 
+
+    public Text scoreText;
+    public Text hiScoreText;
+
+    public float scoreCount;
+    public float hiScoreCount;
+
+    public float pointsPerSecond;
+
+    public bool scoreIncreasing;
+     
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scoreIncreasing)
+        {
+            scoreCount += pointsPerSecond * Time.deltaTime;
+        }
+        scoreCount += pointsPerSecond * Time.deltaTime;
+
+        if(scoreCount > hiScoreCount)
+        {
+            hiScoreCount = scoreCount;
+        }
+
+        scoreText.text = "Score: " + Mathf.Round(scoreCount);
+        hiScoreText.text = "High Score: " + Mathf.Round(hiScoreCount);
+                          
+    }
+}
